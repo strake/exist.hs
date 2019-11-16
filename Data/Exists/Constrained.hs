5075b2e94af2b6b7f379fccaa6b15ad85d53d3d9
M Farkas-Dyck <strake888@gmail.com>
2019-11-15 18:11:48 -0800

    liftE

--- Data/Exists/Constrained.hs
+++ Data/Exists/Constrained.hs
@@ -1,7 +1,10 @@
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
 module Data.Exists.Constrained where
 
 import Data.Typeable
 
 -- | Dependent sum
 data E c a = ∀ k . (Typeable k, c k) => E { unE :: a k }
+
+liftE :: (∀ k . c k => a k -> b k) -> E c a -> E c b
+liftE f (E a) = E (f a)
